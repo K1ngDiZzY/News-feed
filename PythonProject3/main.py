e9778527cecc0dace8bf80f40f2da6b04ed9d3b2
Dillon <120695568+K1ngDiZzY@users.noreply.github.com>
2025-03-23 15:07:18 -0500

    Delete PythonProject3 directory

--- PythonProject3/main.py
+++
@@ -1,8 +0,0 @@
-from HackingNews import NewsFeed
-
-def main():
-    news = NewsFeed()
-    news.print_news()
-
-if __name__ == "__main__":
-    main()
--- PythonProject3/srcs.py
+++
@@ -1,5 +0,0 @@
-
-hacking_rss_list = {
-    'bleepingcomputer': 'https://www.bleepingcomputer.com/feed/',
-    'TheHackerNews_home': 'https://feeds.feedburner.com/TheHackersNews'
-}
